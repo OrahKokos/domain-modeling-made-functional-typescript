619397e8c20d26d8faf619bf636d08f3788ae12c
OrahKokos <marko.paroski.ns@gmail.com>
2021-01-12 17:12:16 +0100

    dev env

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+}
